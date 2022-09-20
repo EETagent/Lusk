1dfe326a31d49e5c0293f4709303d3636142f3bf
EETagent <vojta.jungmann@gmail.com>
2022-09-20 10:39:06 +0200

    feat: přidání Tor.framework

--- Podfile
+++ Podfile
@@ -5,6 +5,7 @@ target 'Lusk' do
 
   # Pods for Lusk
   pod 'HTMLKit', '~> 4.2'
+  pod 'Tor', '~> 407.10.1'
 
   target 'LuskTests' do
     inherit! :search_paths
--- Pods/Target Support Files/Pods-Lusk-LuskUITests/Pods-Lusk-LuskUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Lusk_LuskUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Lusk_LuskUITests
-@end
